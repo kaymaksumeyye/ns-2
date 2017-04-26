fb569691bc42205832fa6a7198bc70b5a5377c91
Sümeyye <sumeyyekaymak41@hotmail.com>
2017-04-26 12:54:29 +0300

    Update AODV_DSDV.tcl

--- AODV_DSDV.tcl
+++ AODV_DSDV.tcl
@@ -1,15 +1,15 @@
 set val(chan)       Channel/WirelessChannel ;
 set val(prop)       Propagation/TwoRayGround ;
 set val(netif)      Phy/WirelessPhy ;
-set val(mac)	      Mac/802_11 ;
-set val(ifq)	      Queue/DropTail ;
-set val(ll)	        LL ;
+set val(mac)	    Mac/802_11 ;
+set val(ifq)	    Queue/DropTail ;
+set val(ll)	    LL ;
 set val(ant)        Antenna/OmniAntenna ;
 set val(ifqlen)	    50 ;
 set val(nn)         5 ;
-set val(rp)	        AODV ;
+set val(rp)	    AODV ;
 set val(x)          1000 ;
-set val(y)	        1000 ;
+set val(y)	    1000 ;
 set val(stop)       150 ;
 set val(tr)         temptr
 	
