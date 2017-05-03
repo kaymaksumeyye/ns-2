87863dc471bfecd15963dd5dfc57245ee315618e
Sümeyye <sumeyyekaymak41@hotmail.com>
2017-05-03 17:08:08 +0300

    Update avgDelay.pl

--- avgDelay.pl
+++ avgDelay.pl
@@ -2,55 +2,38 @@
 #use strict;
 use warnings;
 $openFile=$ARGV[1];
-#$oneLine=$ARGV[2];
 open(Trace, $ARGV[0]);
-$sendTime = 0;
-$recvTime = 0;
-$packet_id = 0;
+@sendTime=(0,0,0);
+@recvTime=(0,0,0);
+@packet_id=(0,0,0);
 $delay = 0;
-$recvPacketCount = 0;
-$totalDelay = 0;
-$avgDelay = 0;
-
+$count=0;
 open (AVGDELAY, ">>avgDelay_$openFile.txt");
 
 while(<Trace>)
 {
 	@line = split; 
-	if(($line[0] eq 's') && ($line[18] eq 'AGT'))
-	{
-		$sendTime = $line[2];
-		$packet_id = $line[40];
-		while(<Trace>)
-		{
-        @line=split;
-		    if(($line[0] eq 'r') && ($line[18] eq 'AGT'))
-		    {
-          if(($line[40] == $packet_id))
-          {
-            $recvTime = $line[2];
-            $recvPacketCount++;
-            $delay = ($recvTime - $sendTime)/1000;
-            $totalDelay = $totalDelay + $delay;
-            print AVGDELAY ("$sendTime $delay\n");
-            last;
-          }
-		    }
+	if(($line[0] eq 's') && ($line[18] eq 'AGT')){
+		$sendTime[$count] = $line[2];
+		$packet_id[$count] = $line[40];	# printf("$packet_id[$count] ");	
+	        $count++;  
+        }
+ 	if(($line[0] eq 'r') && ($line[18] eq 'AGT')){
+	      for($i=0;$i<$count-1;$i++){
+		      if(($line[40] == $packet_id[$i])){
+			    $recvTime[$i] = $line[2]; #printf("$packet_id[$i] $line[40]\n");
+			    last; 
+		      }
+              }
+        }
+}
+for($i=0;$i<$count;$i++){
+     if(defined $recvTime[$i]){
+	$delay = $recvTime[$i] - $sendTime[$i];
+	print AVGDELAY ("$sendTime[$i] $delay\n");
      }
-	}
 }
 
-if($recvPacketCount != 0 )
-{
-	$avgDelay = $totalDelay / $recvPacketCount;
-	$avgDelay = $avgDelay * 1000;
-	#print("\navgDelay = $avgDelay milisecond");
-	#print AVGDELAY ("$oneLine $avgDelay\n");
-}
-else
-{
-	#print("\n Package not received");
-}
 close(Trace);
 close(AVGDELAY);
 exit(0);
